7b6fb1dbf1441ec243ea65a26edb4fcb5fc31909
jay-law <jay-law@users.noreply.github.com>
2022-05-14 17:59:22 -0400

    version testing

--- src/exfill/__init__.py
+++ src/exfill/__init__.py
@@ -3,10 +3,8 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(__file__))
-
 from setuptools_scm import get_version
 
-__version__ = get_version()
+sys.path.append(os.path.dirname(__file__))
 
-# __version__ = "1.1.2"
+__version__ = get_version()
