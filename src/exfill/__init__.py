1c79cb74f8ea382e5415371e706a724a823ebb07
jay-law <jay-law@users.noreply.github.com>
2022-05-14 20:14:32 -0400

    fix version

--- src/exfill/__init__.py
+++ src/exfill/__init__.py
@@ -7,5 +7,5 @@ from setuptools_scm import get_version
 
 sys.path.append(os.path.dirname(__file__))
 
-version = get_version()
+version = get_version(version_scheme="no-guess-dev")
 __version__ = ".".join(version.split(".")[:3])
