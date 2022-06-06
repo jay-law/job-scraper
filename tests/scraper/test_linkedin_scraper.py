674c74adc1c13ccf83510ff2bd885f597d54de10
jay-law <jay-law@users.noreply.github.com>
2022-06-06 16:59:36 -0400

    added tests for web driver

--- tests/scraper/test_linkedin_scraper.py
+++ tests/scraper/test_linkedin_scraper.py
@@ -1,6 +1,7 @@
 from configparser import NoOptionError, NoSectionError
 
 import pytest
+from selenium import webdriver
 
 from exfill.scrapers.linkedin_scraper import LinkedinScraper
 
@@ -12,7 +13,7 @@ def test_scraper_constructur(load_config):
     assert isinstance(scraper, LinkedinScraper)
 
 
-def test_scraper_constructor_exceptions(load_config):
+def test_scraper_constructor_exceptions_options(load_config):
     config = load_config
 
     config.remove_option("Paths", "gecko_driver")
@@ -23,6 +24,20 @@ def test_scraper_constructor_exceptions(load_config):
     with pytest.raises(NoOptionError):
         LinkedinScraper(config)
 
+
+def test_scraper_constructor_exceptions_sections(load_config):
+    config = load_config
+
     config.remove_section("Scraper")
     with pytest.raises(NoSectionError):
         LinkedinScraper(config)
+
+
+def test_browser_init(load_config):
+    config = load_config
+    scraper = LinkedinScraper(config)
+    driver = scraper.browser_init()
+
+    assert isinstance(driver, webdriver.firefox.webdriver.WebDriver)
+
+    driver.close()
